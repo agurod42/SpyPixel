093e4969ca5347b46e89a4074598257484064e09
AndreIglesias <ciglesia@student.42.fr>
2024-04-14 13:38:59 +0200

    ip fix

--- src/spy.py
+++ src/spy.py
@@ -1,8 +1,13 @@
 from flask import Flask, send_file, request
+# from werkzeug.middleware.proxy_fix import ProxyFix
 import datetime
 import urllib.request
+import json
 
 app = Flask(__name__)
+
+# Ensure Flask is configured to handle proxy headers correctly
+# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def index():
@@ -13,15 +18,19 @@ def pixel():
     time = datetime.datetime.now()
     time = datetime.datetime.strftime(time, '%Y-%m-%d %H:%M:%S')
     ua = request.headers.get('User-Agent')
-    ip = request.remote_addr
-    with urllib.request.urlopen("https://geolocation-db.com/jsonp/"+ ip) as url:
+    ip = request.environ['REMOTE_ADDR']
+     
+    # Fetch geolocation data
+    with urllib.request.urlopen(f"https://geolocation-db.com/jsonp/{ip}") as url:
         data = url.read().decode()
         data = data.split("(")[1].strip(")")
-
-    print(f'T:{time} - IP:{data} - User Agent:{ua}\n')
+        geolocation_data = json.loads(data)
+    
+    # Log geolocation data
+    print(f'T:{time} - IP:{ip} - Geolocation:{geolocation_data} - User-Agent:{ua}\n')
 
     with open('/app/log.txt', 'a') as f:
-        f.write(f'T:{time} - IP:{data} - User Agent:{ua}\n')
+        f.write(f'T:{time} - IP:{ip} - Geolocation:{geolocation_data} - User-Agent:{ua}\n')
     return send_file('pixel.png', mimetype='image/png')
 
 if __name__ == '__main__':
